67a0495c4b9a202573c1ae4f0ba58415a516c930
cornelk <github@codejuggle.dj>
2021-12-03 07:10:24 -0600

    debug ci

--- pkg/nes/gui_nogui.go
+++ pkg/nes/gui_nogui.go
@@ -12,5 +12,10 @@ func setupNoGui() (guiRender func() (bool, error), guiCleanup func(), err error)
 		return true, nil
 	}
 	cleanup := func() {}
+
+	// reference to avoid unused linter warning
+	_ = scaleFactor
+	_ = windowTitle
+
 	return render, cleanup, nil
 }
