4e49f48b7aa96333107d1ef92f024afe9c04245d
cornelk <github@codejuggle.dj>
2022-06-20 22:56:18 -0600

    ca65: fix linker settings PRG start address calculation

--- pkg/disasm/ca65/mapper.go
+++ pkg/disasm/ca65/mapper.go
@@ -7,7 +7,7 @@ MEMORY {
     ZP:     start = $00,    size = $100,    type = rw, file = "";
     RAM:    start = $0200,  size = $600,    type = rw, file = "";
     HDR:    start = $0000,  size = $10,     type = ro, file = %%O, fill = yes;
-    PRG:    start = $8000,  size = $%04X,   type = ro, file = %%O, fill = yes;
+    PRG:    start = $%04X,  size = $%04X,   type = ro, file = %%O, fill = yes;
     CHR:    start = $0000,  size = $%04X,   type = ro, file = %%O, fill = yes;
 }
 
@@ -16,9 +16,9 @@ SEGMENTS {
     OAM:        load = RAM, type = bss, start = $200, optional = yes;
     BSS:        load = RAM, type = bss;
     HEADER:     load = HDR, type = ro;
-    CODE:       load = PRG, type = ro, start = $8000;
+    CODE:       load = PRG, type = ro, start = $%04X;
     DPCM:       load = PRG, type = ro, start = $C000, optional = yes;
-    VECTORS:    load = PRG, type = ro, start = $%04X;
+    VECTORS:    load = PRG, type = ro, start = $FFFA;
     TILES:      load = CHR, type = ro;
 }
 `
@@ -26,8 +26,8 @@ SEGMENTS {
 // generateMapperConfig generates a ca65 linker config dynamically based on the passed ROM settings.
 func generateMapperConfig(conf Config) string {
 	prgSize := conf.PRGSize
-	vectorStart := 0xFFFA - (0x8000 - prgSize)
+	prgStart := 0x10000 - prgSize
 
-	generatedConfig := fmt.Sprintf(mapper0Config, prgSize, conf.CHRSize, vectorStart)
+	generatedConfig := fmt.Sprintf(mapper0Config, prgStart, prgSize, conf.CHRSize, prgStart)
 	return generatedConfig
 }
